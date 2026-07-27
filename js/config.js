94671a326689eefce167284a9744fb73c8a0570c
Under25-Campaigns <hridanshu.ruparel-c@under25.club>
2026-07-27 15:19:11 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    API_URL: "",
+    API_URL: "https://script.google.com/macros/s/AKfycbxi9kTFDUSVzGlveWTfzDmgdIOQ-dyGF_dKfGNzNagcDU8GfYlOSF4qKq68hS_WJqXo/exec",
     APP_NAME: "SHEIN Campus Ambassador Dashboard",
     VERSION: "1.0"
 };
